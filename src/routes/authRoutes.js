4ab54ae216e3bb5c94441bde824e918f68848d8e
sanketp2001 <sanket.p@angularminds.in>
2025-02-11 19:00:00 +0530

    added routes for loin ,register, logout, profile and refreshtoken

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,25 @@
 const express = require("express");
-const router = express.Router();
-const authController=require('../controllers/authController')
+const {
+  register,
+  login,
+  logout,
+  refreshToken,
+} = require("../controllers/authController");
+const validate = require("../middlewares/validateMiddleware");
+const {
+  registerValidation,
+  loginValidation,
+} = require("../utils/userValidation");
+const verifyToken = require("../middlewares/authMiddleware");
 
-router.post("/register", authController.registerUser); // Register a new user
-router.post("/login", authController.loginUser); // Login a user
+const router = express.Router();
+
+router.post("/register", validate(registerValidation), register);
+router.post("/login", validate(loginValidation), login);
+router.get("/profile", verifyToken, (req, res) => {
+  res.json({ message: "Protected route", user: req.user });
+});
+router.get("/logout",verifyToken, logout);
+router.get("/refresh-token", refreshToken); // Auto-renews JWT if close to expiry
 
 module.exports = router;
