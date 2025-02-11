a48f83cd7db7077df2a57d5882e994822f8a94cb
sanketp2001 <sanket.p@angularminds.in>
2025-02-11 19:01:43 +0530

    password hashing and comparing

---
+++ src/service/authentication.js
@@ -0,0 +1,12 @@
+const bcrypt = require("bcrypt");
+
+const hashPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+};
+
+const comparePassword = async (candidatePassword, hashedPassword) => {
+  return bcrypt.compare(candidatePassword, hashedPassword);
+};
+
+module.exports = { hashPassword, comparePassword };
