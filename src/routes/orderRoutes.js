30e03e621a46df3e7ec1235c55e315429b8db1ac
sanketp2001 <sanket.p@angularminds.in>
2025-02-11 19:00:34 +0530

    udpates

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const orderController = require('../controllers/orderController');
+const orderController = require("../controllers/orderController");
 
 // Routes for orders
-router.post('/', orderController.createOrder); // Create a new order
-router.get('/', orderController.getAllOrders); // Get all orders
-router.get('/:id', orderController.getOrderById); // Get order by ID
-router.put('/:id', orderController.updateOrderStatus); // Update order status
-router.delete('/:id', orderController.deleteOrder); // Delete an order
+// router.post("/", orderController.createOrder); // Create a new order
+router.get("/", orderController.getAllOrders); // Get all orders
+router.get("/:id", orderController.getOrderById); // Get order by ID
+// router.put("/:id", orderController.updateOrderStatus); // Update order status
+router.delete("/:id", orderController.deleteOrder); // Delete an order
 
 module.exports = router;
