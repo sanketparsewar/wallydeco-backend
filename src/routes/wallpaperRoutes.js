366be81d90f7e7570a5a1fa8f580da616f9277a2
sanketp2001 <sanket.p@angularminds.in>
2025-02-13 19:00:09 +0530

    changes in routes with middleware

--- src/routes/wallpaperRoutes.js
+++ src/routes/wallpaperRoutes.js
@@ -3,19 +3,22 @@ const router = express.Router();
 const {
   addWallpaper,
   updateWallpaper,
-  getAllWallpapers,
+  getWallpapers,
   getWallpaperById,
   deleteWallpaper,
+  getWallpapersByCategory,
 } = require("../controllers/wallpaperController");
 const validate = require("../middlewares/validateMiddleware");
 const {
   WallpaperValidation,
 } = require("../utils/wallpaperValidation");
+const verifyToken = require("../middlewares/authMiddleware");
 
-router.post("/", validate(WallpaperValidation), addWallpaper);
-router.put("/:id", validate(WallpaperValidation), updateWallpaper); // Update a wallpaper
+router.post("/",verifyToken, validate(WallpaperValidation), addWallpaper);
+router.put("/:id",verifyToken, validate(WallpaperValidation), updateWallpaper); // Update a wallpaper
 router.get("/:id", getWallpaperById);
-router.get("/", getAllWallpapers);
-router.delete("/:id", deleteWallpaper);
+router.get("/", getWallpapers);
+router.get("/category/:category", getWallpapersByCategory);
+router.delete("/:id",verifyToken, deleteWallpaper);
 
 module.exports = router;
