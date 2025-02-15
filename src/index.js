5be24c7a0c61fde550a22008567eb6514036365a
Sanket Parsewar <sanket.parsewar97@gmail.com>
2025-02-16 05:20:55 +0530

    updated cors

--- src/index.js
+++ src/index.js
@@ -20,10 +20,13 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:4200", "https://wallydeconew.vercel.app"], // Allow Angular frontend
+    origin: ["http://localhost:4200", "https://wallydeconew.vercel.app"], // Allow Angular frontend (local + live)
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
     credentials: true, // Allow cookies & authentication headers
   })
 );
+
 
 // Database connection
 connectDB
