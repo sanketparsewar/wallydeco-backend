cfa22dc19530e1a6bdffbd8b7f6bc3342c962639
Sanket Parsewar <sanket.parsewar97@gmail.com>
2025-02-16 14:10:51 +0530

    updates

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -60,7 +60,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: None  ,
+      sameSite: None,
       maxAge: 24 * 60 * 60 * 1000,
     });
 
